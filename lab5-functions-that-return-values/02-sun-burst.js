72e724eb2f0cee4f8b64001210f42bd63b15020e
Kuker, Bill <wxk04060@castleton.edu>
2026-07-01 13:30:25 -0400

    remove sunburst

--- lab5-functions-that-return-values/02-sun-burst.js
+++ lab5-functions-that-return-values/02-sun-burst.js
@@ -3,21 +3,7 @@
 //
 // -------------------------------------------------------
 export function draw() {
-    fill(255,255,0);    //Yellow
 
-    let n = 0;
-    while ( n < 12 ){
-        push();
-        rotateZ(n * 360/12);
-        translate(0,150,0);
-        cone(20,50+height(n)/2);
-        pop();
-        n++;
-    }
-
-    //Draw an orange sphere
-    fill(255,150,0);
-    sphere();
 }
 
 //This function RETURNS either 100 of n is even or
